d1bdf1b8f06f5bd35d2e1b42ec19d15b2001d459
Sean Boyer <seanABoyer@gmail.com>
2022-05-29 12:38:23 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">=1.1.3"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_iam_role_policy" "server_mgmt_lambda_assume_role_policy" {
+  name = "server_mgmt_lambda_assume_role_policy"
+  policy = jsonencode(
+      {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "lambda.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role" "server_mgmt_shutdown" {
+  name ="server_mgmt_shutdown"
+  assume_role_policy = aws_iam_role_policy.server_mgmt_lambda_assume_role_policy.policy
+}
+
+resource "aws_lambda_function" "server_mgmt_shutdown_server" {
+  function_name = "shutdown_server"
+  filename = "function.zip"
+  role = aws_iam_role.server_mgmt_shutdown.arn
+  handler = "function.handler"
+  runtime = "python3.9"
+}
