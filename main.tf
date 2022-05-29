537eaa4cb7944d8d8363ec136d78783c78bd4c4c
Sean Boyer <seanABoyer@gmail.com>
2022-05-29 13:31:25 -0500

    create dynamodb table

--- main.tf
+++ main.tf
@@ -42,11 +42,27 @@ resource "aws_iam_role" "server_mgmt" {
   )
 }
 
+#Lets each tf config opt in to being able to be managed when they insert a record into the table
+resource "aws_dynamodb_table" "manageableEC2Instances"{
+  name = "ManageableEC2Instances"
+  hash_key = "ec2ID"
+  read_capacity  = 20
+  write_capacity   = 20
+  attribute {
+    name = "ec2ID"
+    type = "S"
+  }
+  attribute {
+    name = "name"
+    type = "S"
+  }
+}
+
 resource "aws_lambda_function" "server_mgmt_shutdown_server" {
   function_name = "shutdown_server"
   filename = "function_stop.zip"
   role = aws_iam_role.server_mgmt.arn
-  handler = "function.handler"
+  handler = "function_stop.handler"
   runtime = "python3.9"
 }
 
@@ -54,6 +70,6 @@ resource "aws_lambda_function" "server_mgmt_shutdown_server" {
 #   function_name = "start_server"
 #   filename = "function_start.zip"
 #   role = aws_iam_role.server_mgmt.arn
-#   handler = "function.handler"
+#   handler = "function_start.handler"
 #   runtime = "python3.9"
 # }
